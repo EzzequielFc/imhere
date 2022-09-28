a7d6c74bd89c47a032ffe184bf42e855999b75d3
Ezequiel (Zack) <contato.ezequielf@gmail.com>
2022-09-28 11:46:49 -0300

    first commit-start project

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+export function Home(){
+    return(
+        <View style={styles.container}> 
+            <Text style={styles.eventName}>Nome do evento</Text>
+            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+            <View style={styles.form}>
+                <TextInput 
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor="#6B6B6B"
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.textButton}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
